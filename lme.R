6c459f3f42ba04cc758fada749e2c95eaeee40ee
Claudia <68861698+ClaudiaMV@users.noreply.github.com>
2024-09-26 15:30:35 -0400

    lme.R
    Code

--- lme.R
+++ lme.R
@@ -1,18 +1,10 @@
-#Personal predictor2 and predictor1 in the simulation of future events
 
-# Library -----------------------------------------------------------------
-library(psych)
+# Libraries -----------------------------------------------------------------
 library(dplyr)
-library(tidytext)
 library(janitor)
-library(plyr)
-library(tidyverse)
 library(lme4) #main MLM package
-library(mitml) #to produce R-squared
-library(lattice) #plots for diagnostics
-library(Hmisc)
 library(lmerTest)
-library(interactions)
+library(emmeans)
 
 
 db <- read_csv("file_name.csv")
@@ -20,7 +12,9 @@ db <- clean_names(db)
 
 # Variables ---------------------------------------------------------------
 #Random
-participant <- factor(db$random_variable) #assuming your random variable is called "random_variable" in the database
+random1 <- factor(db$random_variable1) #assuming your random variable is called "random_variable" in the database
+random2 <- factor(db$random_variable2)
+
 #Predictors
 predictor1 <- factor(db$predictor1) #Assuming this predictor variable is called "predictor 1" in the database
 predictor2 <- factor(db$predictor2) #Assuming this predictor variable is called "predictor 1" in the database
@@ -63,29 +57,31 @@ if (any(any_na_col)) {
 #Model 4: main effect of predictor1 + main effect of predictor2 + intercepts
 #Model 5: interaction of predictor1 and predictor2 + intercepts 
 
-model1 <- lmer(other_sen ~ 1 + (1|participant), data = db, REML = FALSE)
-model2 <- lmer(other_sen ~ predictor1 + (1|participant), data = db, REML = FALSE)
-model3 <- lmer(other_sen ~ predictor2 + (1|participant), data = db, REML = FALSE)
-model4 <- lmer(other_sen ~ predictor1 + predictor2 + (1|participant), data = db, REML = FALSE)
-model5 <- lmer(other_sen ~ predictor1 * predictor2 + (1|participant), data = db, REML = FALSE)
+model1a <- lmer(output ~ 1 + (1|random1), data = db, REML = FALSE)
+model1b <- lmer(output ~ 1 + (1|random2), data = db, REML = FALSE)
+
+model2 <- lmer(output ~ predictor1 + (1|random1) + (1|random2), data = db, REML = FALSE)
+model3 <- lmer(output ~ predictor2 + (1|random1) + (1|random2), data = db, REML = FALSE)
+model4 <- lmer(output ~ predictor1 + predictor2 + (1|random1) + (1|random2), data = db, REML = FALSE)
+model5 <- lmer(output ~ predictor1 * predictor2 + (1|random1) + (1|random2), data = db, REML = FALSE)
 
 ## Comparisons ------------------------------------------------------------
-anova(model4.osd, model3.osd) #main effect of predictor1
-anova(model4.osd, model2.osd) #main effect of predictor2
-anova(model5.osd, model4.osd) #interaction
+anova(model4, model3) #main effect of predictor1
+anova(model4, model2) #main effect of predictor2
+anova(model5, model4) #interaction
 
 
-# post-hoc comparisons in case of interaction---------------------------------------------------------------
+# Post-hoc comparisons in case of interaction---------------------------------------------------------------
 library(emmeans)
 library(effects)
 library(sjPlot)
 
-emm_other_sen <- emmeans(model5, ~ predictor1 * predictor2) # Obtain EMMs of the model with the interaction
-pairwise_comparisons2 <- pairs(emm_other_sen)# Perform pairwise comparisons
-adjusted_comparisons2 <- summary(pairwise_comparisons2)# Adjust p-values
-results_df2 <- as.data.frame(adjusted_comparisons2)# Convert to a data frame for easier manipulation
-results_df2$Significant <- results_df2$p.value < 0.05# Add a column to indicate significance
-print(results_df2)# Print results with significance indication
+emm <- emmeans(model5, ~ predictor1 * predictor2) # Obtain EMMs of the model with the interaction
+pairwise_comparisons <- pairs(emm)# Perform pairwise comparisons
+adjusted_comparisons <- summary(pairwise_comparisons)# Adjust p-values
+results_df <- as.data.frame(adjusted_comparisons)# Convert to a data frame for easier manipulation
+results_df$Significant <- results_df$p.value < 0.05# Add a column to indicate significance
+print(results_df)# Print results with significance indication
 
 
 
